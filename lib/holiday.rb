55834188bfa02bc2d8e8cc5ad80fe91152b0a62e
Lola Onanuga <slonanuga@gmail.com>
2018-10-02 16:24:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def all_supplies_in_holidays(holiday_hash)
    szn = szn.to_s.capitalize!
    
    holiday.to_s.split("_")
-   holiday.map {|x| x.capitalize!.join(" ")
+   holiday.map {|x| x.capitalize!.join(" ")}
    
    puts holiday
   
